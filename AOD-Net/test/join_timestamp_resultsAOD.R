be4deb52211b94e7d82d89d7db8e6bf1baaf2456
potmadu <potmadu@gmail.com>
2018-02-13 02:44:18 +0700

    update r file for merge data

--- AOD-Net/test/join_timestamp_resultsAOD.R
+++ AOD-Net/test/join_timestamp_resultsAOD.R
@@ -1,18 +1,22 @@
-metadata = read.csv("C:/Datasets/Haze_Gazer/Instagram-Haze/All-Data-Tagged.csv",stringsAsFactors=FALSE);
-results = read.csv("C:/Users/Fox/Documents/GitHub/Haze-Gazer-Analysis/AOD-Net/test/output_aod_all.csv",stringsAsFactors=FALSE);
+metadata_file="E:/Datasets/Haze-Gazer-Analysis/Instagram-Haze/All Data.csv";
+result_file="E:/GitHub/Haze-Gazer-Analysis/AOD-Net/test/output_aod_landscapes.csv";
+
+output_file="E:/GitHub/Haze-Gazer-Analysis/AOD-Net/test/join_aod_landscapes.csv";
+
+metadata = read.csv(metadata_file,stringsAsFactors=FALSE);
+results = read.csv(result_file,stringsAsFactors=FALSE);
 
 metadata$id = paste(metadata$id,".jpg",sep="");
 
-metadata$waktu = as.POSIXlt(as.numeric(metadata$timestamp),origin="1970-01-01");
+#metadata$waktu = as.POSIXlt(as.numeric(metadata$timestamp),origin="1970-01-01");
 
 library(dplyr);
 
-metadata_results = metadata %>%
-select(-waktu) %>%
-left_join(results,c("id"="id_image")) %>%
+metadata_results = results %>%
+left_join(metadata,c("X0"="id")) %>%
 as.data.frame();
 
-metadata_results$waktu = metadata$waktu;
+metadata_results$waktu = as.POSIXlt(as.numeric(metadata_results$timestamp),origin="1970-01-01");
 
-write.csv(metadata_results,"C:/Users/Fox/Documents/GitHub/Haze-Gazer-Analysis/AOD-Net/test/join_results_aod.csv");
+write.csv(metadata_results,output_file);
 
